14614083bb1964332accbd6e59bed995ccc0169c
serrie <mohammadzidane058@gmail.com>
2024-08-21 17:24:56 +0700

    fix

--- server.js
+++ server.js
@@ -1,30 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-const path = require('path'); // Import module 'path'
+const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000; // Gunakan port yang diberikan oleh lingkungan atau 3000
+const port = 3000;
 
-// Middleware untuk mengizinkan akses ke file statis seperti CSS, JS, dan gambar
-app.use(express.static(path.join(__dirname, 'public')));
+const dataPath = path.join(__dirname, 'database/data.json');
+
+// Middleware untuk mengizinkan akses ke file public
+app.use(express.static('public'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/submit', (req, res) => {
+app.post('/', (req, res) => {
   const { Name, Email, Text } = req.body;
-  const data = `\n\nName: ${Name}\nEmail: ${Email}\nMessage: ${Text}`;
 
-  // Simpan data ke file output.txt
-  fs.appendFile('output.txt', data, (err) => {
-    if (err) throw err;
-    console.log('Data has been saved to output.txt');
+  // Baca data yang sudah ada di data.json
+  fs.readFile(dataPath, 'utf8', (err, jsonData) => {
+    if (err) {
+      console.error('Error reading JSON file:', err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    let data = [];
+
+    try {
+      data = JSON.parse(jsonData); // Parse data yang sudah ada
+    } catch (err) {
+      console.error('Error parsing JSON data:', err);
+    }
+
+    // Tambahkan data baru
+    const newEntry = {
+        name: Name,
+        email: Email,
+        message: Text
+    }
+
+    data.push(newEntry);
+
+    // Simpan kembali data ke data.json
+    fs.writeFile(dataPath, JSON.stringify(data, null, 2), 'utf-8', (err) => {
+      if (err) {
+        console.error('Error writing to JSON file:', err);
+        return res.status(500).send('Internal Server Error');
+      }
+
+      console.log('Data has been saved to data.json');
+      res.sendFile(path.join(__dirname, '/index.html'));
+    });
   });
-
-  res.send('Form submitted successfully!');
 });
 
 app.listen(port, () => {
