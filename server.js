cd6f5930e929dac4945d1895a9929116f191db30
serrie <mohammadzidane058@gmail.com>
2024-09-14 10:43:13 +0700

    commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.post('/', (req, res) => {
